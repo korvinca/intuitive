d3ab8d388d7299afc80634224b49ffcfbb1fd7b0
Ivan Korolevskiy <ivan.korolevskiy@gmail.com>
2023-10-19 18:41:01 -0700

    Add AWS creds

--- terraform_aws/provider.tf
+++ terraform_aws/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.aws_region
-  access_key = "YOUR_AWS_ACCESS_KEY"
-  secret_key = "YOUR_AWS_SECRET_KEY"
+  access_key = "YOUR_AWS_ACCESS_KEY" # Comment this line for using GHA
+  secret_key = "YOUR_AWS_SECRET_KEY" # Comment this line for using GHA
 }
